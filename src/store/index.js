c56a9dd45d70d66b0fb4d9cc7c9a9f2a0aed9ba9
13701138894 <13701138894@163.com>
2020-09-29 19:49:23 +0800

    Vue3 项目搭建练习

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,12 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    
+  },
+  getters: {
+    allTime(){
+      return 0
+    }
   },
   mutations: {
   },
---
+++ src/views/Plan/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <create @handlePlan="handlePlan"></create>
+    </div>
+</template>
+
+<script>
+import create from "./create";
+export default {
+    components:{
+        create
+    },
+    setup(){
+        const handlePlan = (plan)=>{
+            console.log(plan)
+            console.log(process.env)
+            // process.env.NODE_ENV = 'production'
+            console.log(process.env)
+            console.log(process.env.VUE_APP_URL)
+        }
+
+        return {
+            handlePlan
+        }
+    }
+}
+</script>
